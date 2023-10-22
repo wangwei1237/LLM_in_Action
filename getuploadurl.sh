ee12c9930bfb253342ea4792b1a08b7aa15e86ef
Wang Wei <wangwei1237@gmail.com>
2023-10-22 20:46:21 +0800

    add release note

--- getuploadurl.sh
+++ getuploadurl.sh
@@ -2,7 +2,7 @@
 
 set -ex
 
-if [ $# -ne 2 ]
+if [ $# -ne 3 ]
 then
     echo "::error the parameters error, please check!!!"
     exit 1
@@ -12,6 +12,7 @@ URL_PREFIX="https://api.github.com/repos/wangwei1237/LLM_in_Action/releases"
 
 version=$1
 token=$2
+notes="$3"
 
 get_release_url="${URL_PREFIX}/tags/${version}"
 upload_url=$(curl -H "Accept: application/vnd.github.v3+json" "${get_release_url}" | grep 'upload_url' | cut -d'"' -f4)
@@ -19,7 +20,7 @@ upload_url=$(curl -H "Accept: application/vnd.github.v3+json" "${get_release_url
 create_release_url="${URL_PREFIX}"
 if [ "$upload_url" = "" ]
 then
-    upload_url=$(curl -X POST -H "Accept: application/vnd.github.v3+json" "${create_release_url}" -H "Authorization: token ${token}" -d "{\"tag_name\":\"${version}\", \"name\":\"Build for ${version}\"}" | grep 'upload_url' | cut -d'"' -f4)
+    upload_url=$(curl -X POST -H "Accept: application/vnd.github.v3+json" "${create_release_url}" -H "Authorization: token ${token}" -d "{\"tag_name\":\"${version}\", \"name\":\"Build for ${version}\", \"body\":\"${notes}\"}" | grep 'upload_url' | cut -d'"' -f4)
 fi
 
 echo $upload_url
