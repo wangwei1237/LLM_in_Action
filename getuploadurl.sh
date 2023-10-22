4687a091a9553668dede4384bf496758ff612fbe
Wang Wei <wangwei1237@gmail.com>
2023-10-22 21:36:18 +0800

    debug for shell

--- getuploadurl.sh
+++ getuploadurl.sh
@@ -13,12 +13,6 @@ URL_PREFIX="https://api.github.com/repos/wangwei1237/LLM_in_Action/releases"
 version=$1
 token=$2
 notes="$3"
-notes=($notes)
-
-echo ${#notes}
-echo ${notes[0]}
-echo ${notes[1]}
-echo ${notes[2]}
 
 get_release_url="${URL_PREFIX}/tags/${version}"
 upload_url=$(curl -H "Accept: application/vnd.github.v3+json" "${get_release_url}" | grep 'upload_url' | cut -d'"' -f4)
@@ -27,7 +21,7 @@ create_release_url="${URL_PREFIX}"
 
 if [ "$upload_url" = "" ]
 then
-    upload_url=$(curl -X POST -H "Accept: application/vnd.github.v3+json" "${create_release_url}" -H "Authorization: token ${token}" -d "{\"tag_name\":\"${version}\", \"name\":\"Build for ${version}\", \"body\":\"${notes}\"}" | grep 'upload_url' | cut -d'"' -f4)
+    upload_url=$(curl -X POST -H "Accept: application/vnd.github.v3+json" "${create_release_url}" -H "Authorization: token ${token}" -d "{\"tag_name\":\"${version}\", \"name\":\"Build for ${version}\"}" | grep 'upload_url' | cut -d'"' -f4)
 fi
 
 if [ "$upload_url" = "" ]
@@ -35,5 +29,9 @@ then
     echo "::error create release error, please check!!!"
     exit 1
 fi
+
+release_note_url="${URL_PREFIX}/generate-notes"
+echo "$notes" > release_note_new.md
+curl -X POST -H "Accept: application/vnd.github.v3+json" -H "Authorization: token ${token}" "${release_note_url}" -d "{\"tag_name\":\"${version}\", \"configuration_file_path\":\"release_note_new.md\"}"
 
 echo $upload_url
