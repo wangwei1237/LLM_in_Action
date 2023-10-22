5400f343733c314dcd9b78fcf6541237e59ff6aa
Wang Wei <wangwei1237@gmail.com>
2023-10-22 21:44:49 +0800

    debug for shell

--- getuploadurl.sh
+++ getuploadurl.sh
@@ -2,7 +2,7 @@
 
 set -ex
 
-if [ $# -ne 3 ]
+if [ $# -ne 4 ]
 then
     echo "::error the parameters error, please check!!!"
     exit 1
@@ -13,6 +13,7 @@ URL_PREFIX="https://api.github.com/repos/wangwei1237/LLM_in_Action/releases"
 version=$1
 token=$2
 notes="$3"
+ROOT="$4"
 
 get_release_url="${URL_PREFIX}/tags/${version}"
 upload_url=$(curl -H "Accept: application/vnd.github.v3+json" "${get_release_url}" | grep 'upload_url' | cut -d'"' -f4)
@@ -31,9 +32,9 @@ then
 fi
 
 release_note_url="${URL_PREFIX}/generate-notes"
-echo "$notes" > release_note_new.md
-pwd
-cat release_note_new.md
-curl -X POST -H "Accept: application/vnd.github.v3+json" -H "Authorization: token ${token}" "${release_note_url}" -d "{\"tag_name\":\"${version}\", \"configuration_file_path\":\"release_note_new.md\"}"
+echo "$notes" > $ROOT/release_note_new.md
+ls -l $ROOT/release_note_new.md
+cat $ROOT/release_note_new.md
+curl -X POST -H "Accept: application/vnd.github.v3+json" -H "Authorization: token ${token}" "${release_note_url}" -d "{\"tag_name\":\"${version}\", \"configuration_file_path\":\"$ROOT/release_note_new.md\"}"
 
 echo $upload_url
